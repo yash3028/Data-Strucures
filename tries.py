b937209e257f193a74723bb045fd25e68d2c8698
yash3028 <shyamakurayashwanth@gmail.com>
2025-01-13 00:05:08 +0530

    updated files

--- tries.py
+++ tries.py
@@ -16,8 +16,8 @@ class Trie:
     def insert(self, element):
         head = self.root
         n = len(element)
-        for char in range(n):
-            idx = self.index(element[char])
+        for char in element:
+            idx = self.index(char)
             if not head.child[idx]:
                 head.child[idx] = self.getNode()
             head = head.child[idx]
@@ -26,12 +26,40 @@ class Trie:
     def search(self, element):
         head = self.root
         n = len(element)
-        for char in range(n):
-            idx = self.index(element[char])
+        for char in element:
+            idx = self.index(char)
             if not head.child[idx]:
                 return False
             head = head.child[idx]  
         return head.end
+    def empty(self,root):
+        for i in root.child:
+            if i!=None:
+                return False
+        return True
+    def delete(self, element):
+        stack = []
+        head = self.root
+        for char in element:
+            idx = self.index(char)
+            if not head.child[idx]:
+                return False
+            stack.append((head, idx))
+            head = head.child[idx]
+        if not head.end:  
+            return False
+        head.end = False  
+        while stack:
+            parent, idx = stack.pop()
+            if self.empty(head) and not head.end:
+                parent.child[idx] = None
+            else:
+                break
+            head = parent
+        return True
+
+
+
 ans = Trie()
 ans.insert('yash')
 ans.insert('wanth')
@@ -40,3 +68,5 @@ print(ans.search('wanth'))
 ans.insert('bqvd')
 print(ans.search('bqvd'))   
 print(ans.search('missing'))  
+ans.delete('wanth')
+print(ans.search('wanth'))
